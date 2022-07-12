68cd58782fa2d9551afda8717632f936c3c268f6
TomtheCoder2 <57863175+TomtheCoder2@users.noreply.github.com>
2022-07-12 14:24:12 +0200

    reformatted code
    Signed-off-by: TomtheCoder2 <57863175+TomtheCoder2@users.noreply.github.com>

--- src/main/java/mindustry/plugin/minimods/Inspector.java
+++ src/main/java/mindustry/plugin/minimods/Inspector.java
@@ -12,46 +12,11 @@ import mindustry.world.Block;
 import mindustry.world.Tile;
 
 public class Inspector implements MiniMod {
-    /** Players with inspector enabled */
+    /**
+     * Players with inspector enabled
+     */
     private ObjectSet<String> players;
-
-    private static class Pos {
-        public int x;
-        public int y;
-
-        @Override
-        public int hashCode() {
-            return this.x * 31 + this.y;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (!(o instanceof Pos)) return false;
-            Pos p = (Pos)o;
-            return this.x == p.x && this.y == p.y;
-        }
-
-        public Pos(Tile tile) {
-            x = tile.x;
-            y = tile.y;
-        }
-    }
-
     private ObjectMap<Pos, TileInfo> tileInfos = new ObjectMap<>();
-
-    private static class TileInfo {
-        public String placedBy;
-        public String destroyedBy;
-        public String configuredBy; 
-
-        public String placedByName;
-        public String destroyedByName;
-        public String configuredByName;
-
-        /** Only set for blocks that are Blocks.air */
-        public Block previousBlock;
-    }
 
     @Override
     public void registerCommands(CommandHandler handler) {
@@ -63,7 +28,7 @@ public class Inspector implements MiniMod {
                 players.add(player.uuid());
             }
 
-            player.sendMessage(GameMsg.info("Inspector", (had ? "Disabled": "Enabled") + " the inspector"));
+            player.sendMessage(GameMsg.info("Inspector", (had ? "Disabled" : "Enabled") + " the inspector"));
         });
     }
 
@@ -71,7 +36,7 @@ public class Inspector implements MiniMod {
     public void registerEvents() {
         Events.on(EventType.BuildSelectEvent.class, event -> {
             if (event.tile == null) return;
-        
+
             String playerName = event.builder.isPlayer() ? event.builder.getPlayer().name : ("unit " + event.builder.type.name);
             String playerUUID = event.builder.isPlayer() ? event.builder.getPlayer().uuid() : null;
 
@@ -103,7 +68,7 @@ public class Inspector implements MiniMod {
         Events.on(EventType.TapEvent.class, event -> {
             if (event.tile == null) return;
             if (!players.contains(event.player.uuid())) return;
-            
+
             String s = "[white]" + event.tile.block().name + "[pink] ([white]" + event.tile.x + "[pink], [white]" + event.tile.y + "[pink])";
             TileInfo info = tileInfos.get(new Pos(event.tile));
             if (info != null) {
@@ -120,5 +85,43 @@ public class Inspector implements MiniMod {
 
             event.player.sendMessage(GameMsg.custom("Inspector", "pink", s));
         });
+    }
+
+    private static class Pos {
+        public int x;
+        public int y;
+
+        public Pos(Tile tile) {
+            x = tile.x;
+            y = tile.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return this.x * 31 + this.y;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Pos)) return false;
+            Pos p = (Pos) o;
+            return this.x == p.x && this.y == p.y;
+        }
+    }
+
+    private static class TileInfo {
+        public String placedBy;
+        public String destroyedBy;
+        public String configuredBy;
+
+        public String placedByName;
+        public String destroyedByName;
+        public String configuredByName;
+
+        /**
+         * Only set for blocks that are Blocks.air
+         */
+        public Block previousBlock;
     }
 }
