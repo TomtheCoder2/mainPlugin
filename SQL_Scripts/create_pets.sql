4850a7634069830eed448c35f928420818a9f338
Allen B <pingguo.ny@gmail.com>
2022-07-19 20:39:52 -0400

    pets :)

--- SQL_Scripts/create_pets.sql
+++ SQL_Scripts/create_pets.sql
@@ -3,5 +3,11 @@ CREATE TABLE pets
     owner VARCHAR,
     name VARCHAR,
     species VARCHAR,
-    color VARCHAR
+    color VARCHAR,
+    eatenCoal BIGINT,
+    eatenCopper BIGINT,
+    eatenLead BIGINT,
+    eatenTitanium BIGINT,
+    eatenThorium BIGINT,
+    eatenBeryllium BIGINT
 );
--- src/main/java/mindustry/plugin/minimods/Pets.java
+++ src/main/java/mindustry/plugin/minimods/Pets.java
@@ -10,6 +10,8 @@ import arc.util.Log;
 import arc.util.Structs;
 import arc.util.Timer;
 import mindustry.Vars;
+import mindustry.content.Blocks;
+import mindustry.content.Items;
 import mindustry.content.UnitTypes;
 import mindustry.entities.Units;
 import mindustry.entities.abilities.Ability;
@@ -23,7 +25,11 @@ import mindustry.plugin.discord.discordcommands.DiscordRegistrar;
 import mindustry.plugin.utils.GameMsg;
 import mindustry.plugin.utils.Rank;
 import mindustry.plugin.utils.Utils;
+import mindustry.type.Item;
+import mindustry.type.ItemStack;
 import mindustry.type.UnitType;
+import mindustry.world.Block;
+import mindustry.world.Tile;
 
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 
@@ -62,13 +68,27 @@ public class Pets implements MiniMod {
         return bestTeam;
     }
 
+
+    @Override
+    public void registerEvents() {
+        for (UnitType unit : Vars.content.units()) {
+            if (unit.itemCapacity <= 0) {
+                unit.itemCapacity = 10;
+            }
+        }
+    }
+
     @Override
     public void registerCommands(CommandHandler handler) {
-        handler.<Player>register("pet", "<name...>", "Spawns a pet", (args, player) -> {
+        handler.<Player>register("pet", "[name...]", "Spawns a pet", (args, player) -> {
             var pets = PetDatabase.getPets(player.uuid());
             if (pets == null || pets.length == 0) {
                 player.sendMessage(GameMsg.error("Pet", "You didn't create any pets. Join our Discord to make a pet."));
                 return;
+            }
+
+            if (args.length == 0) {
+                player.sendMessage(GameMsg.custom("Pet", "sky", Seq.with(pets).toString(", ", p -> p.name)));
             }
 
             var pet = Structs.find(pets, p -> p.name.equalsIgnoreCase(args[0]));
@@ -193,12 +213,34 @@ public class Pets implements MiniMod {
                         ownerName = Utils.escapeEverything(info.lastName);
                     }
 
+                    String foodEaten = "";
+                    Item[] foods = possibleFoods(pet.species);
+                    if (Structs.contains(foods, Items.coal)) {
+                        foodEaten += ":black_large_square: " + pet.eatenCoal + "\n";
+                    }
+                    if (Structs.contains(foods, Items.copper)) {
+                        foodEaten += ":brown_square: " + pet.eatenCopper + "\n";
+                    }
+                    if (Structs.contains(foods, Items.lead)) {
+                        foodEaten += ":purple_square: " + pet.eatenLead + "\n";
+                    }
+                    if (Structs.contains(foods, Items.titanium)) {
+                        foodEaten += ":blue_square: " + pet.eatenTitanium + "\n";
+                    }
+                    if (Structs.contains(foods, Items.thorium)) {
+                        foodEaten += ":red_square: " + pet.eatenThorium + "\n";
+                    }
+                    if (Structs.contains(foods, Items.beryllium)) {
+                        foodEaten += ":green_square: " + pet.eatenBeryllium + "\n";
+                    }
+
                     ctx.sendEmbed(new EmbedBuilder()
                             .setColor(new java.awt.Color(pet.color.r, pet.color.g, pet.color.b))
                             .setTitle("Pet: " + pet.name)
                             .addInlineField("Owner", ownerName)
                             .addInlineField("Species", pet.species.localizedName)
                             .addInlineField("Color", "#" + pet.color.toString())
+                            .addInlineField("Food Eaten", foodEaten.trim())
                     );
                 }
         );
@@ -298,6 +340,18 @@ public class Pets implements MiniMod {
         }
     }
 
+    protected static Item[] possibleFoods (UnitType type) {
+        if (type == UnitTypes.crawler) {
+            return new Item[] { Items.coal };
+        } else if (type == UnitTypes.quasar || type == UnitTypes.pulsar) {
+            return new Item[] { Items.beryllium, Items.titanium, Items.thorium };
+        } else if (type.flying) {
+            return  new Item[] { Items.copper, Items.lead, Items.titanium, Items.thorium };
+        } else {
+            return  new Item[] { Items.copper, Items.lead, Items.titanium };
+        }
+    }
+
     static class PetTeam extends Team {
         public PetTeam(int id, String name, Color color) {
             super(id, name, color);
@@ -313,8 +367,6 @@ public class Pets implements MiniMod {
          */
         int maxVel = 250;
         Unit unit;
-        long prevTime = System.currentTimeMillis();
-        boolean hasLabel = false;
 
         public PetController(Player player, String name) {
             this.player = player;
@@ -337,6 +389,12 @@ public class Pets implements MiniMod {
             var pets = spawnedPets.get(uuid);
             pets.remove(name);
         }
+
+        long prevTime = System.currentTimeMillis();
+        boolean hasLabel = false;
+        boolean isEating = false;
+        // mining subset of eating
+        Tile mining = null;
 
         @Override
         public void updateUnit() {
@@ -378,18 +436,127 @@ public class Pets implements MiniMod {
 
             unit.x += vx * (dt) / 1000f;
             unit.y += vy * (dt) / 1000f;
-            unit.rotation = unit.angleTo(player);
+
+            if (!isEating) {
+                unit.rotation = unit.angleTo(player);
+            }
 
             // boost
             unit.elevation(1f);
 
             // labels
-            if (!hasLabel && Math.abs(vx) < 0.5 && Math.abs(vy) < 0.5) {
+            if (!hasLabel && Math.abs(vx) < 1 && Math.abs(vy) < 1) {
                 Call.label(name, 1f, unit.x, unit.y + 5);
                 hasLabel = true;
                 Timer.schedule(() -> {
                     hasLabel = false;
                 }, 1f);
+            }
+
+            handleFood(Math.abs(vx) < 1 && Math.abs(vy) < 1, dt);
+        }
+
+        private int itemsEaten = 0;
+        private long lastAction = 0;
+        protected void handleFood(boolean isStill, long dt) {
+            // food
+            if (!isEating && !unit.hasItem()) {
+                int startX = unit.tileX() - 5;
+                int startY = unit.tileY() - 5;
+                Seq<Tile> tiles = new Seq<>();
+                for (int x = startX; x < startX + 10; x++) {
+                    for (int y = startY; y < startY + 10; y++) {                        
+                        Tile tile = Vars.world.tiles.get(x, y);
+                        if (tile == null) continue;
+                        if (tile.drop() != null && Structs.contains(possibleFoods(unit.type), tile.drop()) && tile.block() == Blocks.air) {
+                            tiles.add(tile);
+                        }
+                    }
+                }
+
+                Log.info("possible ores: " + tiles.size);
+                if (tiles.size != 0) {
+                    this.mining = tiles.random();
+                    Log.info("mining: " + this.mining.drop());
+                    isEating = true;
+                }
+            }
+
+            if (isEating) {
+                // if `mining` is non-null, mine that tile
+                // additionally increase stack 4/s (1/250ms)
+                if (mining != null) {
+                    unit.mineTile(mining);
+                    if (System.currentTimeMillis() - lastAction > 250) {
+                        if (unit.stack == null || unit.stack.item != mining.drop()) {
+                            unit.stack = new ItemStack(mining.drop(), 1);
+                        }
+                        if (unit.stack.amount < unit.itemCapacity())
+                            unit.stack.amount += 1;
+                        lastAction = System.currentTimeMillis();
+                    }
+                }
+
+                // if mine is too far away, skip to eating
+                if (mining != null && (mining.x - unit.tileX())*(mining.x - unit.tileX()) + (mining.y - unit.tileY())*(mining.y - unit.tileY()) >= 10*10) {
+                    mining = null;
+                    if (unit.stack != null) {
+                        itemsEaten = unit.stack.amount;
+                    } else {
+                        itemsEaten = 0;
+                    }
+                }
+                if (unit.stack != null && unit.stack.amount >= unit.itemCapacity()) {
+                    mining = null;
+                    itemsEaten = unit.stack.amount;
+                }
+
+                // eat
+                if (unit.stack != null && mining == null) {
+                    // eat one item every 500ms
+                    if (System.currentTimeMillis() - lastAction > 500) {
+                        unit.stack.amount -= 1;
+                        unit.heal();
+                        lastAction = System.currentTimeMillis();
+                    }
+
+                    if (unit.stack.amount == 0) {
+                        var item = unit.stack.item;
+                        unit.clearItem();                       
+                        isEating = false;
+
+                        // update database
+                        int amount = itemsEaten;
+                        var pet = Structs.find(PetDatabase.getPets(uuid), p -> p.name.equals(this.name));
+                        if (pet == null) { // pet was deleted
+                            Call.unitDespawn(unit);
+                            return;
+                        }
+                        if (item == Items.coal) {
+                            pet.eatenCoal += amount;
+                        } else if (item == Items.copper) {
+                            pet.eatenCopper += amount;
+                        } else if (item == Items.lead) {
+                            pet.eatenLead += amount;
+                        } else if (item == Items.titanium) { 
+                            pet.eatenTitanium += amount;
+                        } else if (item == Items.thorium) {
+                            pet.eatenThorium += amount;
+                        } else if (item == Items.beryllium) {
+                            pet.eatenBeryllium += amount;
+                        }
+                        PetDatabase.updatePet(pet);
+                    }
+                }
+
+
+                // rotation
+                if (mining != null)
+                    unit.rotation = unit.angleTo(mining);
+                else
+                    // spin 360 degrees/sec
+                    unit.rotation += dt * 360f / 1000f;
+
             }
         }
     }
@@ -405,7 +572,6 @@ class PetDatabase {
             ResultSet rs = ps.executeQuery();
             Seq<Pet> pets = new Seq<>();
             while (rs.next()) {
-                Log.info("new pet");
                 Pet pet = Pet.fromSQL(rs);
                 pets.add(pet);
             }
@@ -420,13 +586,19 @@ class PetDatabase {
      * Inserts a new pet
      */
     public static void addPet(Pet pet) {
-        String sql = "INSERT INTO pets VALUES (?, ?, ?, ?)";
+        String sql = "INSERT INTO pets VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         try {
             PreparedStatement pstmt = Database.conn.prepareStatement(sql);
             pstmt.setString(1, pet.owner);
             pstmt.setString(2, pet.name);
             pstmt.setString(3, pet.species.name);
             pstmt.setString(4, pet.color.toString());
+            pstmt.setLong(5, pet.eatenCoal);
+            pstmt.setLong(6, pet.eatenCopper);
+            pstmt.setLong(7, pet.eatenLead);
+            pstmt.setLong(8, pet.eatenTitanium);
+            pstmt.setLong(9, pet.eatenThorium);
+            pstmt.setLong(10, pet.eatenBeryllium);
 
             pstmt.executeUpdate();
         } catch (SQLException e) {
@@ -438,13 +610,19 @@ class PetDatabase {
      * Updates a given pet, using name and owner as identifiers
      */
     public static void updatePet(Pet pet) {
-        String sql = "UPDATE pets SET color = ?, species = ? WHERE owner = ? AND name = ?";
+        String sql = "UPDATE pets SET color = ?, species = ?, eatenCoal = ?, eatenCopper = ?, eatenLead = ?, eatenTitanium = ?, eatenThorium = ?, eatenBeryllium = ? WHERE owner = ? AND name = ?";
         try {
             PreparedStatement pstmt = Database.conn.prepareStatement(sql);
             pstmt.setString(1, pet.color.toString());
             pstmt.setString(2, pet.species.name);
-            pstmt.setString(3, pet.owner);
-            pstmt.setString(4, pet.name);
+            pstmt.setLong(3, pet.eatenCoal);
+            pstmt.setLong(4, pet.eatenCopper);
+            pstmt.setLong(5, pet.eatenLead);
+            pstmt.setLong(6, pet.eatenTitanium);
+            pstmt.setLong(7, pet.eatenThorium);
+            pstmt.setLong(8, pet.eatenBeryllium);
+            pstmt.setString(9, pet.owner);
+            pstmt.setString(10, pet.name);
 
             pstmt.executeUpdate();
         } catch (SQLException e) {
@@ -476,6 +654,13 @@ class PetDatabase {
         public String name;
         public UnitType species;
         public Color color;
+        
+        public long eatenCoal;
+        public long eatenCopper;
+        public long eatenLead;
+        public long eatenTitanium;
+        public long eatenThorium;
+        public long eatenBeryllium;
 
         public Pet(String owner, String name) {
             this.owner = owner;
@@ -491,12 +676,19 @@ class PetDatabase {
             if (species == null) {
                 return null;
             }
-
             Color color = Color.valueOf(colorStr);
 
             Pet pet = new Pet(owner, name);
             pet.color = color;
             pet.species = species;
+
+            pet.eatenCopper = rs.getLong("eatenCopper");
+            pet.eatenLead = rs.getLong("eatenLead");
+            pet.eatenTitanium = rs.getLong("eatenTitanium");
+            pet.eatenThorium = rs.getLong("eatenThorium");
+            pet.eatenCoal = rs.getLong("eatenCoal");
+            pet.eatenBeryllium = rs.getLong("eatenBeryllium");
+
             return pet;
         }
     }
