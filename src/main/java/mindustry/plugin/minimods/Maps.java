7c21bb4bae8e5cfa1c9d9adc07008f383372925a
Allen B <pingguo.ny@gmail.com>
2022-07-21 18:56:11 -0400

    fix maps

--- src/main/java/mindustry/plugin/minimods/Maps.java
+++ src/main/java/mindustry/plugin/minimods/Maps.java
@@ -163,7 +163,7 @@ public class Maps implements MiniMod {
                     var maps = Database.rankMapRatings(100, 0);
                     int c = 1;
                     for (Database.Map m : maps) {
-                        sb.append(String.format("%-2d | %-4d | %-4d | %-5.1f %s\n", c, m.positiveRating, m.negativeRating, m.positiveRating / (double)(m.positiveRating + m.negativeRating) * 100 + "%", Strings.stripColors(m.name)));
+                        sb.append(String.format("%-2d | %-4d | %-4d | %-5.1f%% %s\n", c, m.positiveRating, m.negativeRating, m.positiveRating / (double)(m.positiveRating + m.negativeRating) * 100, Strings.stripColors(m.name)));
                         c++;
                     }
                     eb.setDescription("```\n" + sb + "\n```");
