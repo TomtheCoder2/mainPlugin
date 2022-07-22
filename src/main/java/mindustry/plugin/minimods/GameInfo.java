4eddb76837b5b056bdc619ca89814578728f52cc
Allen B <pingguo.ny@gmail.com>
2022-07-22 12:18:29 -0400

    remove .info command #24

--- src/main/java/mindustry/plugin/minimods/GameInfo.java
+++ src/main/java/mindustry/plugin/minimods/GameInfo.java
@@ -54,50 +54,6 @@ public class GameInfo implements MiniMod {
 
     @Override
     public void registerDiscordCommands(DiscordRegistrar handler) {
-        handler.register("info", "<player>",
-                data -> {
-                    data.roles = new long[]{Roles.ADMIN, Roles.MOD};
-                    data.help = "Get player information.";
-                    data.category = "Moderation";
-                    data.aliases = new String[]{"i"};
-                },
-                ctx -> {
-                    EmbedBuilder eb = new EmbedBuilder();
-                    Administration.PlayerInfo info = Query.findPlayerInfo(ctx.args.get("player"));
-                    if (info == null) {
-                        ctx.error("Error", "Player '" + ctx.args.get("player") + "' not found or is offline");
-                        return;
-                    }
-                    eb.addField("Times Kicked", info.timesKicked + "");
-                    eb.addField("Current Name", info.lastName);
-                    eb.addField("Current IP", info.lastIP);
-
-                    String names = "";
-                    for (String name : info.names) {
-                        names += name + "\n";
-                    }
-                    eb.addField("Names", names);
-
-                    Database.Player pd = Database.getPlayerData(info.id);
-                    if (pd != null) {
-                        eb.addField("Rank", Rank.all[pd.rank].name);
-                        eb.addField("Play time", pd.playTime + " minutes");
-                        eb.addField("Games", pd.gamesPlayed + "");
-                        eb.addField("Buildings built", pd.buildingsBuilt + "");
-                        if (pd.bannedUntil > Instant.now().getEpochSecond()) {
-                            eb.addField("Banned", "Until " + Utils.epochToString(pd.bannedUntil));
-                        } else if (pd.banned) {
-                            eb.addField("Banned", "Forever");
-                        }
-                        if (pd.banReason != null && !pd.banReason.equals("")) {
-                            eb.addField("Ban Reason", pd.banReason);
-                        }
-                    }
-                    eb.setColor(DiscordPalette.INFO);
-                    ctx.sendEmbed(eb);
-                }
-        );
-
         handler.register("players", "",
                 data -> {
                     data.help = "List online players and their IDs.";
