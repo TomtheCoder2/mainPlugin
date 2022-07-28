ebb86a90da505331fafc27e7a78cb792f89dd293
Allen B <pingguo.ny@gmail.com>
2022-07-28 18:26:10 -0400

    /skipwave - aboslute majority, no timer

--- src/main/java/mindustry/plugin/minimods/Skipwave.java
+++ src/main/java/mindustry/plugin/minimods/Skipwave.java
@@ -1,6 +1,7 @@
 package mindustry.plugin.minimods;
 
 import arc.struct.ObjectIntMap;
+import arc.struct.ObjectSet;
 import arc.util.CommandHandler;
 import mindustry.Vars;
 import mindustry.game.EventType;
@@ -8,12 +9,13 @@ import mindustry.gen.Call;
 import mindustry.gen.Groups;
 import mindustry.gen.Player;
 import mindustry.plugin.MiniMod;
+import mindustry.plugin.utils.Cooldowns;
 import mindustry.plugin.utils.GameMsg;
 import arc.Events;
 
 /** Provides the /skipwave command */
 public class Skipwave implements MiniMod {
-    private ObjectIntMap<String> votes = new ObjectIntMap<>();
+    private ObjectSet<String> votes = new ObjectSet<>();
 
     @Override
     public void registerEvents() {
@@ -27,30 +29,38 @@ public class Skipwave implements MiniMod {
     }
 
     private static int requiredVotes() {
-        return Groups.player.size() <= 3 ? 2 : 3;
+        return Groups.player.size() / 2 + 1;
     }
 
     @Override
     public void registerCommands(CommandHandler handler) {
+        Cooldowns.instance.set("skipwave", 5);
         handler.<Player>register("skipwave", "[y/n]", "Skip wave of enemies", (args, player) -> {
-            int vote = 1;
+            if (!Cooldowns.instance.canRun("skipwave", player.uuid())) {
+                player.sendMessage(GameMsg.ratelimit("Skip", "skipwave"));
+            }
+            boolean vote = true;
             if (args.length > 0) {
                 switch (args[0]) {
-                    case "y", "yes": vote = 1; break;
-                    case "n", "no": vote = -1; break;
+                    case "y", "yes": vote = true; break;
+                    case "n", "no": vote = false; break;
                     default:
                         player.sendMessage(GameMsg.error("Skipwave", "Second argument must be 'yes' or 'no'"));
                         return;
                 }
             }
 
-            votes.put(player.uuid(), vote);
+            if (vote) {
+                votes.add(player.uuid());
+            } else {
+                votes.remove(player.uuid());
+            }
 
-            int totalVotes = votes.values().toArray().sum();
-            Call.sendMessage(GameMsg.info("Skipwave", "[orange]" + player.name() + "[lightgray] has voted to " + 
-                (vote == 1 ? "skip" : "not skip") + " the wave " + "(" + totalVotes + "/" + requiredVotes() + ")"));
+            int totalVotes = votes.size;
+            Call.sendMessage(GameMsg.info("Skip", "[white]" + player.name() + "[" + GameMsg.INFO + "] has voted to " + 
+                (vote ? "skip" : "not skip") + " the wave " + "(" + totalVotes + "/" + requiredVotes() + "). Type [" + GameMsg.CMD + "]/skipwave[] to agree to skip the wave."));
             if (totalVotes >= requiredVotes()) {
-                Call.sendMessage(GameMsg.success("Skipwave", "Vote passed. Skipping wave."));
+                Call.sendMessage(GameMsg.success("Skip", "Vote passed. Skipping wave."));
                 Vars.logic.runWave();
                 votes.clear();
             }
