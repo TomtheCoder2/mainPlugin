6582dd48ab466266a28cc3ff174cbf53482bd4c3
TomtheCoder2 <57863175+TomtheCoder2@users.noreply.github.com>
2023-02-24 15:46:46 +0100

    remove map render, cause it creates problems
    Signed-off-by: TomtheCoder2 <57863175+TomtheCoder2@users.noreply.github.com>

--- src/main/java/mindustry/plugin/minimods/Maps.java
+++ src/main/java/mindustry/plugin/minimods/Maps.java
@@ -180,13 +180,13 @@ public class Maps implements MiniMod {
                             .setAuthor(ctx.author().getDisplayName(ctx.server()), ctx.author().getAvatar().getUrl().toString(), ctx.author().getAvatar().getUrl().toString())
                             .setColor(DiscordPalette.WARN)
                             .addInlineField("Size", meta.getInt("width") + "x" + meta.getInt("height"));
-                    try {
-                        eb.setImage(ContentServer.renderRaw(data));
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        System.out.println("Failed to render map preview");
-                        return;
-                    }
+//                    try {
+//                        eb.setImage(ContentServer.renderRaw(data));
+//                    } catch (Exception e) {
+//                        e.printStackTrace();
+//                        System.out.println("Failed to render map preview");
+//                        return;
+//                    }
                     new MessageBuilder()
                             .addEmbed(eb)
                             .addAttachment(data, "Map " + Strings.stripColors(meta.get("name")) + ".msav")
@@ -281,11 +281,11 @@ public class Maps implements MiniMod {
                                 .addInlineField("Shortest Game", md.shortestGame + " min")
                                 .addInlineField("Total Play Time", md.playTime + " min");
                     }
-                    try {
-                        eb.setImage(ContentServer.renderMap(found));
-                    } catch (Exception e) {
-                        System.out.println("Failed to render map");
-                    }
+//                    try {
+//                        eb.setImage(ContentServer.renderMap(found));
+//                    } catch (Exception e) {
+//                        System.out.println("Failed to render map");
+//                    }
 
                     ctx.reply(
                             new MessageBuilder()
