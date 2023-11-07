dd8a7e01879c62402ba900f35c43f86257a4795f
Mars <49356012+7003Mars@users.noreply.github.com>
2023-11-07 21:25:52 +0800

    Anti bot measures? v2

--- src/main/java/mindustry/plugin/minimods/NoBots.java
+++ src/main/java/mindustry/plugin/minimods/NoBots.java
@@ -2,16 +2,13 @@ package mindustry.plugin.minimods;
 
 import arc.Events;
 import arc.files.Fi;
-import arc.struct.IntSet;
-import arc.util.Http;
 import arc.util.Log;
+import arc.util.Reflect;
 import arc.util.Strings;
-import arc.util.Time;
-import arc.util.serialization.Jval;
 import mindustry.Vars;
 import mindustry.game.EventType.ConnectionEvent;
 import mindustry.gen.Call;
-import mindustry.gen.Groups;
+import mindustry.net.Net;
 import mindustry.net.NetConnection;
 import mindustry.plugin.MiniMod;
 import mindustry.plugin.database.Database;
@@ -22,105 +19,78 @@ import org.javacord.api.entity.message.MessageAttachment;
 import java.util.List;
 
 public class NoBots implements MiniMod {
-	Fi filePath = Vars.dataDirectory.child("mods").child("asn-ban.csv");
+	Fi filePath = Vars.dataDirectory.child("mods").child("ipv4.txt");
+	static int[] ips;
+	static short[] masks;
 	public NoBots() {
-		// TODO: File path config
 		parseBlacklist(filePath);
-	}
-
-	public static IntSet asnBlacklist = new IntSet();
-	static IpChecker[] checkers = {
-			new IpChecker() {
-				long cooldownTime = 0;
-				@Override
-				public void check(String ip) {
-					if (Time.millis() < cooldownTime) {
-						checkers[1].check(ip);
-						return;
-					}
-					Log.info("Sending http://ip-api.com/json/@?fields=18432", ip);
-					Http.get("http://ip-api.com/json/"+ ip + "?fields=18432").submit(res -> {
-						if (Strings.parseInt(res.getHeader("X-Rl")) == 0) {
-							// Untested code above
-							cooldownTime = Time.millis() + Strings.parseInt(res.getHeader("X-Ttl")) * 1000L;
-						}
-						String asnString = Jval.read(res.getResultAsString()).getString("as");
-						Log.info("Asn string: @", asnString);
-						int asn = Strings.parseInt(asnString.substring(2, asnString.indexOf(" ")));
-						Log.info("@, @", asn, asnBlacklist.contains(asn));
-						if (asnBlacklist.contains(asn)) kickAll(ip);
-					});
-				}
-			},
-			ip -> {
-				Http.get("https://ipapi.co/" + ip + "/json").submit(res -> {
-					String asnString = Jval.read(res.getResultAsString()).getString("asn");
-					int asn = Strings.parseInt(asnString.substring(2));
-					if (asnBlacklist.contains(asn)) kickAll(ip);
-				});
-			}
-	};
-
-	public static void kickAll(String ip) {
-		// TODO: Could be possible that player isn't created yet?
-		Vars.netServer.admins.blacklistDos(ip);
-		Groups.player.each(p -> {
-			if (p.con.address.equals(ip)) {
-				Call.kick(p.con, "This ip is blocked, try disabling any vpn\ndiscord.phoenix-network.dev");
-				p.con.close();
-			}
-		});
 	}
 
 	public static int parseBlacklist(Fi file) {
 		if (!file.exists()) {
-			Log.err("No asn blacklist file specified");
+			Log.err("No ip blacklist file specified");
 			return -1;
 		}
-		String[] asns = file.readString().split("\n");
-		for (String line : asns) {
-			int end = line.indexOf(",");
-			if (end == -1) end = line.length();
-			asnBlacklist.add(Strings.parseInt(line.substring(0, end)));
+		String[] lines = file.readString().split("\n");
+		ips = new int[lines.length];
+		masks = new short[lines.length];
+		for (int i = 0; i < lines.length; i++) {
+			String line = lines[i];
+			int slash = line.indexOf('/');
+			String subnet = line.substring(0, slash);
+			int maskBits = 32-Strings.parseInt(line.substring(slash+1));
+			ips[i] = convertIp(subnet) >>> maskBits;
+			masks[i] = (short) maskBits;
 		}
-		Log.info("Loaded a total of @ asns", asnBlacklist.size);
-		return asnBlacklist.size;
+
+		Log.info("Loaded a total of @ ips", ips.length);
+		return ips.length;
 	}
 
 
 	@Override
 	public void registerEvents() {
 		Events.on(ConnectionEvent.class, connectionEvent -> {
+			// The code is probably blocking and a long db query could tank tps
 			NetConnection con = connectionEvent.connection;
 			Database.Player pd = Database.getPlayerData(con.uuid);
 			if (pd != null && pd.verified) return;
-			checkers[0].check(con.address);
+			int ip = convertIp(con.address);
+			for (int i = 0; i < ips.length; i++) {
+				if (ips[i] == (ip >>> masks[i])) {
+					Call.kick(con, "This ip is blocked, try disabling any vpn\ndiscord.phoenix-network.dev");
+					con.close();
+					return;
+				}
+			}
 		});
 	}
 
 	@Override
 	public void registerDiscordCommands(DiscordRegistrar handler) {
-		handler.register("update-asn", "",
+		handler.register("update-ip-blacklist", "",
 				data -> {
-					data.usage = ".csv file";
+					data.usage = ".txt file";
 					data.roles =  new long[]{Roles.ADMIN, Roles.MOD, Roles.APPRENTICE};
 				},
 				ctx -> {
 					List<MessageAttachment> attachments = ctx.event.getMessageAttachments();
-					if (attachments.size() != 1 || (!attachments.get(0).getFileName().endsWith("csv"))) {
-						ctx.error("Provide a csv file", "");
+					if (attachments.size() != 1 || (!attachments.get(0).getFileName().endsWith("txt"))) {
+						ctx.error("Provide a txt file", "");
 					}
 					attachments.get(0).downloadAsByteArray().thenAccept(b -> {
 						filePath.writeBytes(b);
 						int size = parseBlacklist(filePath);
-						ctx.success("Updated asn blacklist", "Size: " + size);
+						ctx.success("Updated ip blacklist", "Size: " + size);
 					});
 				});
 	}
-}
 
-@FunctionalInterface
-interface IpChecker {
-	void check(String ip);
+	static int convertIp(String ip) {
+		int d1 = ip.indexOf('.'), d2 = ip.indexOf('.', d1+1), d3 = ip.indexOf('.', d2+1);
+		return Strings.parseInt(ip, 10, 0, d3+1, ip.length()) |
+				Strings.parseInt(ip, 10, 0, d2+1, d3) << 8 |
+				Strings.parseInt(ip, 10, 0, d1+1, d2) << 16 |
+				Strings.parseInt(ip, 10, 0, 0, d1) << 24;
+	}
 }
-
